3bb6d952a4bf6d25f91267526b98ae86ad33278a
FikriAminuddin <93188126+FikriAminuddin@users.noreply.github.com>
2021-12-08 18:30:53 +0800

    Create a new website
    Create a new folder Lab Assignment 2

---
+++ Lab Assignment 2/adjustvolume.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("myMusic");
+
+        function setHalfVol() { 
+          x.volume = 0.2;
+          alert("The volume is 15% which is Low !!");
+        }
+
+        function setFullVol() { 
+          x.volume = 1.0;
+          alert("The volume is 100% which is Full Volume !!");
+        }
